c0a9b53087681b8862c7608b6aaff4108b0ad1cd
jack mclaughlin <jack.mclaughlin24@gmail.com>
2021-12-21 23:50:53 -0500

    Revert "changes aspect ratio, removes background"
    This reverts commit f71657760f87dea1f790909ccf36c8c1ae3f0cdf.

--- Player/PlayerScene.gd
+++ Player/PlayerScene.gd
@@ -1,14 +1,27 @@
 extends Node2D
 
-
+signal start_game
 signal update_highscore(score)
 
+
+var filepath = "res://Player/keybindings.ini"
+var configFile
+
+func _ready() -> void:
+	configFile = ConfigFile.new()
+	if configFile.load(filepath) == OK:
+		for key in configFile.get_section_keys("keybinds"):
+			var key_value = configFile.get_value("keybinds", key)
+			print(key, " : ", OS.get_scancode_string(key_value))
+	else:
+		print("configFile not found")
+		get_tree().quit()
 
 func _on_TileMap_missed_next_tile(score) -> void:
 	emit_signal("update_highscore", score)
 
 
 func _on_MainHUD_start_game() -> void:
-	$PlayerBody.start_game()
+	emit_signal("start_game")
 	$PlayerCamera.start_game()
 	$MusicPlayer.start_game()
