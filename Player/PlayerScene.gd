f71657760f87dea1f790909ccf36c8c1ae3f0cdf
jack mclaughlin <jack.mclaughlin24@gmail.com>
2021-12-21 23:49:16 -0500

    changes aspect ratio, removes background

--- Player/PlayerScene.gd
+++ Player/PlayerScene.gd
@@ -1,27 +1,14 @@
 extends Node2D
 
-signal start_game
+
 signal update_highscore(score)
 
-
-var filepath = "res://Player/keybindings.ini"
-var configFile
-
-func _ready() -> void:
-	configFile = ConfigFile.new()
-	if configFile.load(filepath) == OK:
-		for key in configFile.get_section_keys("keybinds"):
-			var key_value = configFile.get_value("keybinds", key)
-			print(key, " : ", OS.get_scancode_string(key_value))
-	else:
-		print("configFile not found")
-		get_tree().quit()
 
 func _on_TileMap_missed_next_tile(score) -> void:
 	emit_signal("update_highscore", score)
 
 
 func _on_MainHUD_start_game() -> void:
-	emit_signal("start_game")
+	$PlayerBody.start_game()
 	$PlayerCamera.start_game()
 	$MusicPlayer.start_game()
--- World/Main.gd
+++ World/Main.gd
@@ -1,10 +1,6 @@
 extends Node2D
 
 
-func _on_MainHUD_start_game() -> void:
-	$Player/PlayerBody.start_game()
-
-
 func _on_Player_update_highscore(score) -> void:
 	$MainHUD.update_high_score(score)
 
