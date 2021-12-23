0f41a4c98c3acc1aeac733ec50f7dd5af570c4b0
jack mclaughlin <jack.mclaughlin24@gmail.com>
2021-12-23 16:52:53 -0500

    limits how far out horizontally tiles can be placed

--- Player/TileDrawer.gd
+++ Player/TileDrawer.gd
@@ -4,9 +4,14 @@ signal missed_next_tile(score)
 signal update_counter(steps)
 signal orient_player(direction)
 
+export var startTile = Vector2(3,-1)
+var maxR = (startTile.x + 10)
+var maxL = (startTile.x - 10)
+
 var curTile
 var tileArray
 var stepCounter
+
 
 const RIGHTV = Vector2(1,-3)
 const LEFTV = Vector2(-1,-3)
@@ -14,41 +19,45 @@ const LEFTV = Vector2(-1,-3)
 
 func _ready() -> void:
 	randomize()
-	curTile = Vector2(3,-1)
+	curTile = startTile
 	stepCounter = 0
 	
 	# makes 25 random steps at the beginning
 	tileArray = []
 	for n in 15:
-		var dir = randf()
-		var tile = randi() % 4
-		if dir > .5:
-			var newSpot = curTile + RIGHTV
-			set_cellv(newSpot, tile)
-			curTile = newSpot
-			tileArray.append("r")
-		if dir < .5:
-			var newSpot = curTile + LEFTV
-			set_cellv(newSpot, tile)
-			curTile = newSpot
-			tileArray.append("l")
+		place_randomized_tile()
 	
 	emit_signal("orient_player", tileArray.front())
 
 
-func _on_player_moved(direction: String, _position) -> void:
+func place_randomized_tile() -> void:
 	var dir = randf()
 	var tile = randi() % 4
+	
 	if dir > .5:
 		var newSpot = curTile + RIGHTV
-		set_cellv(newSpot, tile)
-		curTile = newSpot
-		tileArray.append("r")
+		if newSpot.x < maxR:
+			place_tile("r", newSpot, tile)
+		else:
+			newSpot = curTile + LEFTV
+			place_tile("l", newSpot, tile)
 	if dir < .5:
 		var newSpot = curTile + LEFTV
-		set_cellv(newSpot, tile)
-		curTile = newSpot
-		tileArray.append("l")
+		if newSpot.x > maxL:
+			place_tile("l", newSpot, tile)
+		else:
+			newSpot = curTile + RIGHTV
+			place_tile("r", newSpot, tile)
+
+
+func place_tile(dir: String, newSpot: Vector2, texture_index: int) -> void:
+	set_cellv(newSpot, texture_index)
+	tileArray.append(dir)
+	curTile = newSpot
+
+
+func _on_player_moved(direction: String, _position) -> void:
+	place_randomized_tile()
 	
 	if not direction == tileArray.pop_front():
 		emit_signal("missed_next_tile", stepCounter)
