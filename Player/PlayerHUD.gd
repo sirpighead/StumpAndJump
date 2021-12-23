79578b2100748959261f0a6907a39af723863638
jack mclaughlin <jack.mclaughlin24@gmail.com>
2021-12-23 17:56:03 -0500

    adds setting menu, volume slider

--- Player/PlayerHUD.gd
+++ Player/PlayerHUD.gd
@@ -2,10 +2,14 @@ extends CanvasLayer
 
 signal restart_game
 signal mute
+signal change_volume(volume)
+
+var settings_showing
 
 func _ready() -> void:
 	$RestartButton.hide()
 	$EndMessage.hide()
+	$Settings.hide()
 
 func _on_TileMap_update_counter(steps) -> void:
 	$MoveCounter.set_text(str(steps))
@@ -37,3 +41,18 @@ func _on_MuteButton_pressed() -> void:
 
 func _on_TileMap_orient_player(direction) -> void:
 	$Direction.set_text(direction.to_upper())
+
+
+func _on_TextureButton_pressed() -> void:
+	settings_showing = !settings_showing
+	
+	if settings_showing: 
+		$Settings.show()
+	else: 
+		$Settings.hide()
+
+
+func _on_VolumeSlider_value_changed(value: float) -> void:
+	emit_signal("change_volume", value)
+	
+	$Settings/VolumeRow/VolumeLabel.set_text("Volume: " + str(int(value)))
--- World/MusicPlayer.gd
+++ World/MusicPlayer.gd
@@ -12,3 +12,9 @@ func _on_HUD_mute() -> void:
 	muted = !muted
 	if muted: set_stream_paused(true)
 	else: set_stream_paused(false)
+
+
+func _on_HUD_change_volume(volume) -> void:
+	var newVol = int(volume/100.0 * 33) - 50
+	if newVol < -49: newVol = -100
+	set_volume_db(newVol)
