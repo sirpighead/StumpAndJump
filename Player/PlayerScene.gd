ee3294e069a35103893bc4cd19a887f858812cd4
jack mclaughlin <jack.mclaughlin24@gmail.com>
2021-12-25 11:13:30 -0500

    removes useless function

--- Player/PlayerScene.gd
+++ Player/PlayerScene.gd
@@ -4,19 +4,6 @@ signal start_game
 signal update_highscore(score)
 
 
-var filepath = "res://Player/keybindings.ini"
-var configFile
-
-func _ready() -> void:
-	configFile = ConfigFile.new()
-	if configFile.load(filepath) == OK:
-		for key in configFile.get_section_keys("keybinds"):
-			var key_value = configFile.get_value("keybinds", key)
-			print(key, " : ", OS.get_scancode_string(key_value))
-	else:
-		print("configFile not found")
-		get_tree().quit()
-
 func _on_TileMap_missed_next_tile(score) -> void:
 	emit_signal("update_highscore", score)
 
